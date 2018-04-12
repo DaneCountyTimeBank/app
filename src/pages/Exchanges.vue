dd89928752752048e0e9ba8e407259257aaaf203
Andrew Trusty <andrew@kulacauses.com>
2018-04-11 22:12:58 -0500

    extract debounce function

--- src/pages/Exchanges.vue
+++ src/pages/Exchanges.vue
@@ -67,23 +67,10 @@
 
 <script>
 
+    import { debounce } from '../utils';
     import Timebank from '../timebank';
     import InfiniteLoading from 'vue-infinite-loading';
 
-    function debounce(func, wait, immediate) {
-        var timeout;
-        return function() {
-            var context = this, args = arguments;
-            var later = function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-            var callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
-        };
-    }
 
     export default {
         name: 'Exchanges',
